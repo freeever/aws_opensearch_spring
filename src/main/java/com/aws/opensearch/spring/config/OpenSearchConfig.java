846684e86877c7cad862500b67f39d97c082ea16
Daniel Xu <daniel.xu2@ontario.ca>
2022-06-19 22:47:35 -0400

    Change some configurations to check the environment variables before checking properties

--- src/main/java/com/aws/opensearch/spring/config/OpenSearchConfig.java
+++ src/main/java/com/aws/opensearch/spring/config/OpenSearchConfig.java
@@ -1,5 +1,6 @@
 package com.aws.opensearch.spring.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -9,9 +10,11 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class OpenSearchConfig {
@@ -31,17 +34,21 @@ public class OpenSearchConfig {
     @Value("${opensearch.password}")
     private String password;
 
+    @Autowired
+    private Environment _env;
+
     @Bean
     public CredentialsProvider credentialsProvider() {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(getUsername(), getPassword()));
 
         return credentialsProvider;
     }
 
     @Bean
     public RestHighLevelClient client() {
-        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, port, scheme))
+        RestClientBuilder builder = RestClient.builder(new HttpHost(getHostname(), getPort(), getScheme()))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
                     public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
@@ -51,4 +58,28 @@ public class OpenSearchConfig {
         return new RestHighLevelClient(builder);
     }
 
+    public String getHostname() {
+        String envHost = _env.getProperty("OPENSEARCH_HOST");
+        return StringUtils.isBlank(envHost) ? hostname : envHost;
+    }
+
+    public String getScheme() {
+        String envScheme = _env.getProperty("OPENSEARCH_SCHEME");
+        return StringUtils.isBlank(envScheme) ? scheme : envScheme;
+    }
+
+    public int getPort() {
+        String envPort = _env.getProperty("OPENSEARCH_PORT");
+        return StringUtils.isBlank(envPort) ? port : Integer.valueOf(envPort);
+    }
+
+    public String getUsername() {
+        String envUser = _env.getProperty("OPENSEARCH_USER");
+        return StringUtils.isBlank(envUser) ? username : envUser;
+    }
+
+    public String getPassword() {
+        String envPass = _env.getProperty("OPENSEARCH_PASSWORD");
+        return StringUtils.isBlank(envPass) ? password : envPass;
+    }
 }
